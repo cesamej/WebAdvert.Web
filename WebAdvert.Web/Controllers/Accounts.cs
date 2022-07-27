ff8649d2e5b0094be08e765e8a5c45238977f1c4
cmejiaAutosled <103968599+cmejiaAutosled@users.noreply.github.com>
2022-07-27 18:09:52 -0300

    First Push

---
+++ WebAdvert.Web/Controllers/Accounts.cs
@@ -0,0 +1,116 @@
+﻿using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Runtime.Internal.Transform;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _cognitoUserPool;
+        public Accounts(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager,
+            CognitoUserPool cognitoUserPool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _cognitoUserPool = cognitoUserPool;
+        }
+       
+        public async Task<IActionResult> SignUp()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupModel signupModel )
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _cognitoUserPool.GetUser(signupModel.Email);
+                if (user == null) {
+                    ModelState.AddModelError("UserExist", "User with this eamil alredy exist");
+                    return View(signupModel); 
+                }
+                user.Attributes.Add(CognitoAttribute.Name.ToString(), signupModel.Email);
+                var createdUser = await _userManager.CreateAsync(user, signupModel.Password);
+                if (createdUser.Succeeded)
+                {
+                    RedirectToAction("Confirm");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm(ConfirmModel confirmModel)
+        {
+            return View(confirmModel);
+        }
+        [HttpPost]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> Confirm_Post(ConfirmModel confirmModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(confirmModel.Email);
+                if(user == null)
+                {
+                    ModelState.AddModelError("NotFound", "User with given email does not exist");
+                    return View(confirmModel);
+                }
+
+                var result = await (_userManager as CognitoUserManager<CognitoUser>).
+                    ConfirmSignUpAsync(user, confirmModel.Code, true).ConfigureAwait(false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                    return View(confirmModel);
+                }
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(LoginModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public async Task<IActionResult> LoginPost(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email,
+                    model.Password, model.RememberMe, false).ConfigureAwait(false);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+                ModelState.AddModelError("LoginError", "Email and password do not match");
+            }
+
+            return View("Login", model);
+        }
+
+        public async Task<IActionResult> Signout()
+        {
+            if (User.Identity.IsAuthenticated) await _signInManager.SignOutAsync().ConfigureAwait(false);
+            return RedirectToAction("Login");
+        }
+
+    }
+}
